cfb9c4500307d7fecbc568706371b6f1e5b09ae8
Caemasar <caemasar@protonmail.com>
2017-10-09 14:55:25 +0800

    【web开发】-->>[。]-->>准备进入【koa入门】

---
+++ c_8_web_koa.js
@@ -0,0 +1,12 @@
+var koa = require('koa');
+var app = koa();
+
+app.use('/test', function *() {
+    yield doReadFile1();
+    var data = yield doReadFile2();
+    this.body = data;
+});
+
+app.listen(8014);
+
+// 并不成功。。。原因未知
